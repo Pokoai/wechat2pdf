21d4530f2d788f4ef34b6096e68698b9faa4024e
pokoai <pokeusr@outlook.com>
2022-07-04 04:58:51 +0800

    功能基本上已实现

--- album2pdf/find_all_links.py
+++ album2pdf/find_all_links.py
@@ -1,8 +1,70 @@
 #! python
 # find_all_links.py - 通过合集链接找到该合集下所有文章的链接、标题等信息
 
+"""
+功能基本实现 2022-7-4
+
+"""
+
 import requests, bs4
+import re
 import os
+
+
+# 单独爬取合集页面里第一篇（倒序）文章ID、标题、链接、序列号、发布时间
+# 顺便爬取微信公众号名称、合集名称
+# 将以上内容写入文件data.txt中，后续可改为写入数据库中或csv
+# 同时返回第一篇文章id、data.txt文件夹路径
+def get_first_msg_info(album_url, output_dir_path):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+    }
+
+    res = requests.get(album_url, headers)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text)
+
+    # 获取微信公众号名称
+    wechat_name_elem = soup.select('.album__author-name')
+    wechat_name = wechat_name_elem[0].getText()
+
+    # 获取合集名称
+    album_name_elem = soup.select('#js_tag_name')
+    album_name = album_name_elem[0].getText().replace('#', '')
+
+    # 获取第一篇文章信息
+    first_elem = soup.select('li')[0]
+    first_id = first_elem.get('data-msgid')
+    pos_num = first_elem.get('data-pos_num')
+    title = first_elem.get('data-title')
+    link = first_elem.get('data-link')
+
+    # 获取第一篇文章发布时间
+    # 发布时间是用 JavaScript 渲染后显示的，要用正则表达式截取时间戳
+    # ![](https://img.arctee.cn/one/202207021312051.png)
+    publish_time = 0  # 默认值
+    res = requests.get(link, headers)
+    res.raise_for_status()
+    match = re.search(r'\{e\(0,(.*),0,document.getElementById\("publish_time"\)\)', res.text, re.S)
+    if match:
+        publish_time = int(match.group(1).split('"')[1])
+
+    # 根据合集名称确定路径
+    album_path = os.path.join(output_dir_path, album_name)  # 合集文件夹路径
+    os.makedirs(album_path, exist_ok=True)
+    file_path = os.path.join(album_path, 'data.txt')  # data.txt路径
+
+    # 写入第一篇文章的信息
+    with open(file_path, 'w', encoding='utf-8') as f:
+        # f.write(f'公众号名称：{wechat_name}\n合集名称：{album_name}\n合集本地路径：{album_path}\n\n'
+        #         f'序列：{pos_num}\n文章标题：{title}\n发布时间：{publish_time}\n'
+        #         f'链接：{link}\nID：{first_id}\n\n')
+
+        f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{publish_time}\n'
+                f'链接：{link}\nID：{first_id}\n\n')
+
+    return first_id, file_path
+
 
 # 设置requests.get()的参数params
 # 目前仅自动获取first_id, 其他参数手动提供，后续改进为全部自动获取
@@ -27,76 +89,55 @@ def set_params(first_id):
     }
     return params
 
+
 # 获取合集内所有文章标题、链接、发布时间、ID等信息
-def get_all_info(url, output_dir_path):
+# requests.get(url=url, headers=headers, params=params)的响应信息不包含第一篇文章，所有需要单独爬取
+def get_all_info(album_url, output_dir_path):
     """
-    url：合集链接
+    album_url：合集链接
     output_dir_path：存储文章信息的文件所在文件夹
     """
+    # 先将第一篇文章信息写入文件中，并获得其id和合集名称
+    first_id, file_path = get_first_msg_info(album_url, output_dir_path)
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
     }
+    # 通过网络抓包获取的url
+    url = 'https://mp.weixin.qq.com/mp/appmsgalbum'
 
-    # 先获取最新文章ID
-    res = requests.get(url, headers)
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text)
-    firstIdElem = soup.select('li')[0]
-    first_id = firstIdElem.get('data-msgid')  # 合集页面里排在第一位的文章id
-    # print(first_id)
-
-    # 通过网络抓包获取的urll
-    urll = 'https://mp.weixin.qq.com/mp/appmsgalbum'
-    params = set_params(first_id)
-    data_json = requests.get(url=urll, headers=headers, params=params).json()
-
-    # 这里应该将获取的数据写到csv文件中或者数据库里
-    # 我暂时先写到data.txt文件里
-    file_path = os.path.join(output_dir_path, 'data.txt')
     msg_cnt = 0  # 记录爬取到的文章数量
-
-    # 解析返回的json数据
-    album_resp = data_json.get('getalbum_resp')
-    article_list = album_resp.get('article_list')
-    for dic in article_list:
-        pos_num = dic['pos_num']
-        title = dic['title']
-        # print(title)
-        url = dic['url']
-        create_time = dic['create_time']
-        msgid = dic['msgid']
-        msg_cnt += 1
-    # print(msg_cnt)
-
-        # 写入的文章不含最新的第一篇，也可能不包含最后的几篇
-        with open(file_path, 'a+', encoding='utf-8') as f:
-            f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{create_time}\n链接：{url}\nID：{msgid}\n\n')
-
-    # 检查最后一篇文章的pos_num是否为1来判断爬取的文章是否齐全
-    # 不为1则从new_id接着爬取，这里仅爬取两次，改进版可以通过循环检测爬取
-    if int(article_list[msg_cnt-1]['pos_num']) != 1:
-        new_id = article_list[msg_cnt-1]['msgid']
-        first_id = new_id
+    while True:
         params = set_params(first_id)
-        data_json = requests.get(url=urll, headers=headers, params=params).json()
-
+        data_json = requests.get(url=url, headers=headers, params=params).json()
+        # 解析返回的json数据
         album_resp = data_json.get('getalbum_resp')
         article_list = album_resp.get('article_list')
-        # msg_cnt += len(article_list)  # 总共爬取到的文章数量
         for dic in article_list:
             pos_num = dic['pos_num']
             title = dic['title']
-            url = dic['url']
-            create_time = dic['create_time']
+            link = dic['url']
+            publish_time = dic['create_time']
             msgid = dic['msgid']
+            msg_cnt += 1
 
+            # 写入的文章不含最新的第一篇，也可能不包含最后的几篇
             with open(file_path, 'a+', encoding='utf-8') as f:
-                f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{create_time}\n链接：{url}\nID：{msgid}\n\n')
+                f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{publish_time}\n链接：{link}\nID：{msgid}\n\n')
+
+        # 如果本次最后一篇文章的pos_num为1则跳出循环
+        if int(article_list[msg_cnt-1]['pos_num']) == 1:
+            break
+        first_id = article_list[msg_cnt-1]['msgid']  # 更新first_id
+        msg_cnt = 0  # msg_cnt需要清零，为下次循环准备
+
+    return file_path
+
 
 
 if __name__ == "__main__":
-    url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIzNTQ4ODg4OA==&action=getalbum&album_id=2206783352551063553&scene=173&from_msgid=2247487451&from_itemidx=1&count=3&nolastread=1#wechat_redirect'
+    album_url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIzNTQ4ODg4OA==&action=getalbum&album_id=2206783352551063553&scene=173&from_msgid=2247487451&from_itemidx=1&count=3&nolastread=1#wechat_redirect'
     output_dir_path = "D:\Media\Desktop\wechat2pdf"  # 主文件夹路径
-    get_all_info(url, output_dir_path)
+    get_all_info(album_url, output_dir_path)
 
     print("Done!")
--- album2pdf/wechat2pdf_v0.3.py
+++ album2pdf/wechat2pdf_v0.3.py
@@ -8,14 +8,15 @@
 后续改进：
 1. 自动获取公众号全部合集链接，然后手动选取感兴趣的合集
 2. 用 pyqt5 封装起来
-3. 多线程采集（仅仅为了学习实践，实际上没必要，因为一次性采集文章不多）
+3. 多线程，提高采集速度
 """
 
 import pdfkit
 import requests, bs4
-import os, sys
-import re, time
-import json
+import os, time, re
+
+from find_all_links import get_all_info
+
 
 # 模板html,微信抓取到的html内容过多
 T_HTML = """
@@ -74,147 +75,42 @@ def timestamp_convert_localdate(timestamp, time_format="%Y-%m-%d"):
     return styleTime
 
 
-# 获取合集内所有文章标题、链接、发布时间、ID等信息
-def get_all_info(url, output_dir_path):
-    """
-    url：合集链接
-    output_dir_path：存储文章信息的文件所在文件夹
-    """
-
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
-    }
-
-    # 先获取最新文章ID
-    res = requests.get(url, headers)
-    res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text)
-    firstIdElem = soup.select('li')[0]
-    first_id = firstIdElem.getText('data-msgid')  # 合集页面里排在第一位的文章id
-
-    # 网络抓包获取的
-    urll = 'https://mp.weixin.qq.com/mp/appmsgalbum'
-    # 目前仅自动获取first_id, 其他参数手动提供，后续改进为全部自动获取
-    params = {
-        'action': 'getalbum',
-        '_biz': 'MzIzNTQ4ODg4OA==',
-        'album_id': '2206783352551063553',
-        'count': '50',
-        'begin_msgid': first_id,
-        'begin_itemidx': '1',
-        'uin': '',
-        'key': '',
-        'pass_ticket': '',
-        'wxtoken': '',
-        'devicetype': '',
-        'clientversion': '',
-        '_biz': 'MzIzNTQ4ODg4OA==',
-        'appmsg_token': '',
-        'x5': '0',
-        'f': 'json',
-    }
-    data_json = requests.get(urll, headers, params).json()
-
-    album_resp = data_json.get('getalbum_resp')
-    article_list = album_resp.get('article_list')
-    msg_cnt = len(article_list)  # 爬取到的文章数量
-    for dic in article_list:
-        pos_num = dic['pos_num']
-        title = dic['title']
-        url = dic['url']
-        create_time = dic['create_time']
-        msgid = dic['msgid']
-
-        # 这里应该将获取的数据写到csv文件中或者数据库里
-        # 我暂时先写到data.txt文件里
-        file_path = os.path.join(output_dir_path, 'data.txt')
-        # 写入的文章不含最新的第一篇，也可能不包含最后的几篇
-        with open(file_path, 'a+', encoding='utf-8') as f:
-            f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{create_time}\n链接：{url}\nID：{msgid}\n\n')
-
-    # 检查最后一篇文章的pos_num是否为1来判断爬取的文章是否齐全
-    # 不为1则从new_id接着爬取，这里仅爬取两次，改进版可以通过循环检测爬取
-    if article_list[msg_cnt-1]['pos_num'] != 1:
-        new_id = article_list[msg_cnt-1]['msgid']
-        first_id = new_id
-        data_json = requests.get(urll, headers, params).json()
-
-        album_resp = data_json.get('getalbum_resp')
-        article_list = album_resp.get('article_list')
-        msg_cnt += len(article_list)  # 总共爬取到的文章数量
-        for dic in article_list:
-            pos_num = dic['pos_num']
-            title = dic['title']
-            url = dic['url']
-            create_time = dic['create_time']
-            msgid = dic['msgid']
-
-            with open(file_path, 'a+', encoding='utf-8') as f:
-                f.write(f'序列：{pos_num}\n文章标题：{title}\n发布时间：{create_time}\n链接：{url}\nID：{msgid}\n\n')
-
-
 # 替换图片src、元素，否则图片无法显示
 def replace_html_tags(html):
     # 替换图片标签属性
+    # 这种替换方式太过耗时，后续改进可考虑换其他方式
     html = html.replace(
         "data-src", "src").replace('style="visibility: hidden;"', "")
 
-    soup = bs4.BeautifulSoup(html, 'html.parser')
-
-    # 删除评论和投票的html标签
-    if soup.iframe:
-        soup.iframe.decompose()
-
-    # 用模板格式化
-    comments = soup.findAll("img", {"class": "like_comment_pic"})
-    styles = soup.find_all('style')
-    content = soup.find('div', id='page-content')
-    fmt_html = T_HTML.format(style=styles[0].string, content=content)
+    # soup = bs4.BeautifulSoup(html, 'html.parser')
+    #
+    # # 删除评论和投票的html标签
+    # if soup.iframe:
+    #     soup.iframe.decompose()
+    #
+    # # 用模板格式化
+    # comments = soup.findAll("img", {"class": "like_comment_pic"})
+    # styles = soup.find_all('style')
+    # content = soup.find('div', id='page-content')
+    # fmt_html = T_HTML.format(style=styles[0].string, content=content)
     # cnt_html = fmt_html.replace(comments[0].attrs['src'], '')
-
     return html
 
 
-# 获取文章标题
-# 需要借助爬虫提取文章标题
-# 可以改进：无需每篇文章单独爬取，可以一次性获取的
-def get_title(res):
-    soup = bs4.BeautifulSoup(res.text)
-
-    # 标题
-    titleElem = soup.select('#activity-name')
-    title = titleElem[0].getText().split('\n')[2]
-
-    # 公众号名称
-    wechatNameElem = soup.select('#js_name')
-    wechatName = wechatNameElem[0].getText().split()[0]
-
-    # 发布时间
-    # 正则表达式，截取需要的时间戳
-    # ![](https://img.arctee.cn/one/202207021312051.png)
-    match = re.search(r'\{e\(0,(.*),0,document.getElementById\("publish_time"\)\)', res.text, re.S)
-    if match:
-        # print(match.group(1))
-        timestamp = int(match.group(1).split('"')[1])
-    publishTime = timestamp_convert_localdate(timestamp)
-
-    pdfTitle = wechatName + '-' + publishTime + '-' + title
-    return pdfTitle
-
-
-def print_info(pdfTitle, output_path):
+# 终端实时输出转换信息
+def print_info(pdf_title, msg_path):
     print('#' * 100)
-    if 'img' in output_path:
+    if 'img' in msg_path:
         print("Img文章已生成！")
     else:
         print("文章已生成！")
-    print("标题：" + pdfTitle)
-    print("地址：" + output_path)
+    print("标题：" + pdf_title)
+    print("地址：" + msg_path)
     print('#' * 100)
     # print('\n')
 
 
-# 通知文章更新状态（简易版）
+# 查询文章更新状态（简易版），如果未更新则不执行转pdf程序
 # 首先在存储pdf文档的文件夹下手动新建一个文档cnt.txt，内容为空
 def notice_new_title(title_num, output_dir_path):
     """
@@ -248,57 +144,64 @@ def notice_new_title(title_num, output_dir_path):
 
 
 # 生成pdf
-def wechat2pdf(url, category, dir_path="D:\Media\Desktop\wechat2pdf"):
+def wechat2pdf(album_url, output_dir_path="D:\Media\Desktop\wechat2pdf"):
     """
-    url：微信公众号合集链接
-    category：分类子文件夹
+    album_url：微信公众号合集链接
     dir_path：主文件夹，有默认值
     """
-    # 新建文件夹
-    output_dir_path = os.path.join(dir_path, category)
-    # mkdir(output_dir_path)
-    """
-    以递归的方式创建文件夹，如果dir_1不存在，就先创建dir_1，而后递归创建剩余的文件夹，这样就不存在FileNotFoundError；
-    如果想要创建的目录已经存在，设置exist_ok = True，就不会引发FileExistsError
-    """
-    os.makedirs(output_dir_path, exist_ok=True)
+    start_time = time.time()
 
-    link_list = get_all_links()
-    title_num = len(link_list)  # 文章数量
+    # 通知合集文章更新状态
+    # notice_new_title(title_num, output_dir_path)
 
-    for link in link_list:
-        res = requests.get(link)
+    file_str= ''
+    file_path = get_all_info(album_url, output_dir_path)
+    # file_path = "D:\Media\Desktop\wechat2pdf\孟岩投资实证（2022）\data.txt"
+    album_path = os.path.dirname(file_path)
+    with open(file_path, encoding='utf-8') as f:
+        file_str = f.read()
+
+    # 匹配序列号
+    pos_num_list = re.findall(r'序列：(\d+)\s', file_str)
+    # 匹配标题
+    title_list = re.findall(r'文章标题：(.+)\s', file_str)
+    # 匹配发布时间
+    publish_time_list = re.findall(r'发布时间：(\d+)\s', file_str)
+    # 匹配链接
+    link_list = re.findall(r'链接：(\S+)\s', file_str)
+
+    msg_num = len(link_list)  # 文章数量
+    for i in range(msg_num):
+        # 设置文章路径
+        publish_time = timestamp_convert_localdate(int(publish_time_list[i]))
+        title = pos_num_list[i] + '.' + title_list[i] + '-' + publish_time
+        msg_path = os.path.join(album_path, title + '.pdf')
+        msg_path2 = os.path.join(album_path, title + '(img).pdf')  # 带图片
+
+        # requests加载文章
+        res = requests.get(link_list[i])
         res.raise_for_status()
-
         # 转换html
         cnt_html = replace_html_tags(res.text)
-        # 设置标题
-        pdfTitle = get_title(res)
-
-        # 设置文章存储路径
-        output_path = os.path.join(output_dir_path, pdfTitle + '.pdf')  # 无图片
-        output_path2 = os.path.join(output_dir_path, pdfTitle + '(img).pdf')  # 有图片
 
         # 利用pdfkit开始生成pdf文档
-        pdfkit.from_url(url, output_path)  # 无图片
-        print_info(pdfTitle, output_path)
-
+        # pdfkit.from_url(link_list[i], msg_path)  # 无图片版
+        # print_info(title, msg_path)
         try:
-            pdfkit.from_string(cnt_html, output_path2)  # 有图片
+            pdfkit.from_string(cnt_html, msg_path)  # 有图片版
         except IOError:
             pass
-        print_info(pdfTitle, output_path2)
+        print_info(title, msg_path2)
 
-    print("本次共生成 %d 篇文章！\n" % title_num)
-    # 通知文章更新状态
-    notice_new_title(title_num, output_dir_path)
-
+    end_time = time.time()
+    all_time = int(end_time - start_time)
+    print(f"\n本次共生成 {msg_num} 篇文章！")
+    print(f"耗时：{all_time}秒")
 
 
 if __name__ == "__main__":
-    # url：合集链接
-    url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIzNTQ4ODg4OA==&action=getalbum&album_id=1689138318304690182&scene=173&from_msgid=2247487438&from_itemidx=1&count=3&nolastread=1#wechat_redirect'
-    category = '孟岩投资实证2021'  # 类别，根据类别自动创建子文件夹
-    dir_path = "D:\Media\Desktop\wechat2pdf"  # 主文件夹路径
-    wechat2pdf(url, category, dir_path)
+    # album_url：合集链接
+    album_url = 'https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIzNTQ4ODg4OA==&action=getalbum&album_id=2206783352551063553&scene=173&from_msgid=2247487451&from_itemidx=1&count=3&nolastread=1#wechat_redirect'
+    output_dir_path = "D:\Media\Desktop\wechat2pdf"  # 主文件夹路径
+    wechat2pdf(album_url, output_dir_path)
 
