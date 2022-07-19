68739aa5c22433ae43728a111258c14253b16726
Pokoai <pokeusr@outlook.com>
2022-07-19 22:58:27 +0800

    代码重构

--- album2pdf/run_daily.py
+++ album2pdf/run_daily.py
@@ -4,80 +4,75 @@
 import re, time, os
 
 from album2pdf_v1 import wechat2pdf
-from find_all_links import get_first_msg_info
-from find_all_links import get_all_info
-from update_logs import get_history_nums, get_update_status, update_logs
+from find_all_links import get_first_post_info
+from find_all_links import update_db
+from update_logs import read_logs, get_update_status, update_logs
 
 
+######################## 需要自己提供的 ############################################
 # 自己感兴趣的合集链接
-album_url_dict = {
+album_dict = {
     '日记': "https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIwMTIzNDMwNA==&action=getalbum&album_id=2461687967875416065&scene=173&from_msgid=2653411356&from_itemidx=1&count=3&nolastread=1#wechat_redirect",
     '长赢指数投资计划': "https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIwMTIzNDMwNA==&action=getalbum&album_id=2467166481575985152&scene=173&from_msgid=2653411345&from_itemidx=1&count=3&nolastread=1#wechat_redirect",
     '孟岩投资实证（2022）': "https://mp.weixin.qq.com/mp/appmsgalbum?__biz=MzIzNTQ4ODg4OA==&action=getalbum&album_id=2206783352551063553&scene=173&from_msgid=2247487451&from_itemidx=1&count=3&nolastread=1#wechat_redirect",
 
 }
 # 合集名称数据
-album_name_list = list(album_url_dict.keys())
+album_name_list = list(album_dict.keys())
 # 最外层文件夹地址
-output_dir_path = "D:\Media\Desktop\wechat2pdf"
+output_path = "D:\Media\Desktop\wechat2pdf"
 # 日志路径
-logs_path = os.path.join(output_dir_path, '更新日志.txt')
+logs_path = os.path.join(output_path, '更新日志.txt')
+#################################################################################
 
 
 # 更新所有合集 data.txt 数据库文件
-def update_all_db(album_url_dict):
-    print("################### 开始更新：数据库data.txt ###################")
-    for album in album_url_dict.items():
+def update_all_db(album_dict):
+    print("###################### 开始更新：数据库 ######################")
+    for album in album_dict.items():
         album_url = album[1]
         print(f"----------- 正在更新：{album[0]}合集 数据库 -----------")
-        get_all_info(album_url, output_dir_path)
-    print("################# 更新完成：所有数据库data.txt  #################")
+        update_db(album_url, output_path)
+    print("###################### 完成更新：数据库 ######################")
 
 
 # # 下载所有合集所有文章
 # def down_all_album():
 #     for album in album_url_dict.items():
 #         album_url = album[1]
-#         # album_id = re.search(r'album_id=(\d+)&', album_url).group(1)  # 通过链接提取合集id
-#         # print(f'\nalbum_id：{album_id}\n')
+
 #         print(f"\n################### 开始下载 「{album[0]}合集」 ###################\n")
-#         wechat2pdf(album_url, output_dir_path)
+#         wechat2pdf(album_url, output_path, 0)  # 下载所有文章
 #     print("\n####################### 所有合集增量下载完成 #######################\n")
 
 
-# 仅下载已更新的合集的最新文章
+# 下载已更新的合集的最新文章
 # 因为公众号每天只能发布一篇文章，所以只要运行一次 down_all_album，以后每天运行 down_update_album 即可保持文章同步更新
-def down_update_album(album_url_dict, update_flg):
-    album_list = list(album_url_dict.items())
-    # print(type(album_list))
+def down_update_album(album_dict, update_flg):
+    album_list = list(album_dict.items())
 
     for i in range(len(update_flg)):
-        if update_flg[i] == 1:  # 标志位为1才启动下载，否则跳过
+        if update_flg[i] == 1:  # 标志位为1才下载
             album_url = album_list[i][1]
-            # print(type(album_url))
+
             print(f"\n################### 开始下载 「{album_list[i][0]}合集」 ###################\n")
-            wechat2pdf(album_url, output_dir_path)
+            wechat2pdf(album_url, output_path, 1)  # 仅下载第一篇文章
     print("\n####################### 所有合集增量下载完成 #######################")
 
 
 
-if __name__ == "__main__":
+def run_daily():
     start_time = time.time()
 
-    # 1. 找到合集所有链接，更新所有合集 data.txt 数据库文件
-    update_all_db(album_url_dict)
+    # 1. 更新所有合集 data.txt 数据库文件
+    update_all_db(album_dict)
 
-    # 2. 读取日志文件，与各合集数据库文件对比，获取更新状态
-    logs, update_flg, update_cnt = get_update_status(
-        output_dir_path, album_name_list, get_history_nums(logs_path))
-    # print(update_flg)
+    # 2. 更新日志文件，并获取更新标志位、更新文章数量
+    update_flg, update_cnt = update_logs(logs_path, output_path, album_name_list)
 
-    # 3. 更新日志 update_logs.txt 文件
-    update_logs(logs, logs_path)
-
-    # 4. 增量下载文章，但时如果所有合集均未发布新文章，则不用下载
+    # 3. 增量下载文章，如果所有合集均未发布新文章，则不用下载
     if 1 in update_flg:
-        down_update_album(album_url_dict, update_flg)
+        down_update_album(album_dict, update_flg)
 
         end_time = time.time()
         all_time = int(end_time - start_time)
@@ -88,3 +83,7 @@ if __name__ == "__main__":
     else:
         print("\n本次更新状态：\n所有合集没有新文章，无需下载！")
 
+
+
+if __name__ == "__main__":
+    run_daily()
--- album2pdf/update_logs.py
+++ album2pdf/update_logs.py
@@ -6,8 +6,8 @@ import os, re, time
 from album2pdf_v1 import mkfile
 
 
-# 读取日志文件，获取各合集文章历史数量
-def get_history_nums(logs_path):
+# 读取日志文件，获取合集文章历史数量
+def read_logs(logs_path):
     # 若文件不存在即创建
     mkfile(logs_path)
 
@@ -20,36 +20,26 @@ def get_history_nums(logs_path):
         # 这里总结下：re.search()只匹配第一个找到的，re.findall()匹配所有找到的并返回一个列表
         match1 = re.search(r'孟岩投资实证（2022）：共(\d+)篇，', logs_str)
         if match1:
-            mengyan_num = int(match1.group(1))  # 孟岩投资实证（2022）文章数量
+            mengyan_nums = int(match1.group(1))  # 孟岩投资实证（2022）文章数量
         else:
-            mengyan_num = 0  # 初始为0
+            mengyan_nums = 0  # 初始为0
 
         match2 = re.search(r'日记：共(\d+)篇，', logs_str)
         if match2:
-            ediary_num = int(match2.group(1))  # E大日记文
+            ediary_nums = int(match2.group(1))  # E大日记文
         else:
-            ediary_num = 0  # 初始为0
+            ediary_nums = 0  # 初始为0
 
         match3 = re.search(r'长赢指数投资计划：共(\d+)篇，', logs_str)
         if match3:
-            efache_num = int(match3.group(1))  # E大发车文章数量
+            efache_nums = int(match3.group(1))  # E大发车文章数量
         else:
-            efache_num = 0  # 初始为0
+            efache_nums = 0  # 初始为0
 
-    return ediary_num, efache_num, mengyan_num
+    return ediary_nums, efache_nums, mengyan_nums
 
 
-# 读取各合集数据库文件，与日志文件对比，获取更新状态
-def get_update_status(output_dir_path, album_name_list, history_nums):
-    logs = []  # 暂存新日志字符串到该列表中
-    # print(len(album_name_list))
-    # 合集数量
-    album_num = len(album_name_list)
-
-    # 合集更新标志位，默认未更新为0
-    update_flg = [0 for i in range(album_num)]
-
-    # 今日更新标志位
+# 今日更新标志位
     # 因为一天运行5次，每次都会覆盖掉日志文件，但我只会晚上看1次状态，那么只要这5次中有一次更新过，就要记录下来，表示今天更新过
     # 另外因为公众号限制，每天只能发布一篇文章，那么只要更新过，即只更新了一篇
     # 我想在日志文件中记录下今天哪些合集更新了文章或者删除了文章，以便通知大家
@@ -58,19 +48,29 @@ def get_update_status(output_dir_path, album_name_list, history_nums):
     # 3. 发挥日志文件的正常功能：即使用添加写入，而非覆盖写入。这就要改变 正则表达式从日志中提取最新文章数量的代码，打算每次在文件开头添加写入。代码不复杂；
     # 综上所述，采用第3种方案开搞。
 
+
+# 读取各合集数据库文件，与日志文件对比，获取更新状态
+def get_update_status(output_path, album_name_list, history_nums):
+    logs = []  # 暂存新日志字符串到该列表中
+    # print(len(album_name_list))
+
+    # 合集数量
+    album_num = len(album_name_list)
+    # 合集更新标志位，默认未更新为0
+    update_flg = [0 for i in range(album_num)]
     # 更新文章总数
     update_cnt = 0
 
     for i in range(album_num):
-        # 合集数据库地址
-        db_path = os.path.join(output_dir_path, album_name_list[i], 'data.txt')
+        # 数据库地址
+        db_path = os.path.join(output_path, album_name_list[i], 'data.txt')
         with open(db_path, encoding='utf-8') as f:
-            data_str = f.readline()
+            db_str = f.readline()
 
             # 获取第一个序列号，即文章最新数量
-            max_num = int(re.search(r'序列：(\d+)\s', data_str).group(1))
+            album_post_nums = int(re.search(r'序列：(\d+)\s', db_str).group(1))
             # 最新数量与历史数量之差即为更新数量
-            update_num = max_num - history_nums[i]  # 这里需要i
+            update_num = album_post_nums - history_nums[i]  # 这里需要i
 
             # 判断更新状态
             if update_num > 0:
@@ -83,7 +83,7 @@ def get_update_status(output_dir_path, album_name_list, history_nums):
                 update_str = '，删除 ' + str(abs(update_num)) + ' 篇\n'
 
             # 拼接起来，最终要实现效果：「孟岩投资实证（2022）：共28篇，更新 4 篇」
-            log_new_str = album_name_list[i] + '：共' + str(max_num) + '篇' + update_str
+            log_new_str = album_name_list[i] + '：共' + str(album_post_nums) + '篇' + update_str
             # 先加入到日志列表中，等全部加入后，再一次性写入到日志文件中，实现更新日志的效果
             logs.append(log_new_str)
     # print(logs)
@@ -93,11 +93,19 @@ def get_update_status(output_dir_path, album_name_list, history_nums):
 
 
 # 将更新状态写入日志文件中
-def update_logs(logs, logs_path):
+def update_logs(logs_path, output_path, album_name_list):
+    print("\n##################### 开始更新：日志文件 #####################")
+    # 读取日志文件，获取合集文章历史数量
+    history_nums = read_logs(logs_path)  # ediary_nums, efache_nums, mengyan_nums
+
+    # 获取更新状态
+    logs, update_flg, update_cnt = \
+        get_update_status(output_path, album_name_list, history_nums)
+
     # 获取当前时间
     current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-    # 写入文件
+    # 写入日志文件
     with open(logs_path, 'r+', encoding='utf-8') as f:
         # 从文件头部插入
         old = f.read()
@@ -108,12 +116,12 @@ def update_logs(logs, logs_path):
         # 再写入更新日志
         for log in logs:
             f.write(log + '\n')
-
         # 写入原内容
         f.write('\n' + old)
+    print("\n##################### 完成更新：日志文件 #####################")
 
-    # return update_flg, update_cnt  # 将更新标志位、更新总数继续传递下去
-    print("\n##################### 日志文件 更新完成 #####################")
+    return update_flg, update_cnt  # 将更新标志位、更新总数传递下去
+
 
 
 
@@ -127,11 +135,9 @@ if __name__ == "__main__":
     # 合集名称数据
     album_name_list = list(album_url_dict.keys())
     # 最外层文件夹地址
-    output_dir_path = "D:\Media\Desktop\wechat2pdf"
+    output_path = "D:\Media\Desktop\wechat2pdf"
+    logs_path = os.path.join(output_path, 'update_logs.txt')
 
-    logs_path = os.path.join(output_dir_path, 'update_logs.txt')
-    history_nums = get_history_nums(logs_path)
-    logs = get_update_status(output_dir_path, album_name_list, history_nums)
-    update_logs(logs, logs_path)
 
+    update_logs(logs_path, output_path, album_name_list)
     print("日志更新完成!")
